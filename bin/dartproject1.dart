acb9cea391cf6f98402c753185a82cad7722e433
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2023-10-06 07:56:35 -0300

    Update dartproject1.dart

--- bin/dartproject1.dart
+++ bin/dartproject1.dart
@@ -13,29 +13,28 @@ void main(List<String> arguments) {
     ];
   }
 
-  final user_score = 0;
-  final CPU_score = 0;
+  var user_score = 0;
+  var CPU_score = 0;
   print('Enter the number of rounds you want to play: ');
   int? num_round = int.parse(stdin.readLineSync()!);
   for (var i = 0; i < num_round; i++) {
+    print("Round ${i + 1}");
     final user = roll_dice();
     final user_result = user.reduce((a, b) => a + b);
     final CPU = roll_dice();
     final CPU_result = CPU.reduce((a, b) => a + b);
     print('Dice roled by user: $user \t Dice roled by CPU: $CPU');
+    print("User resuls: $user_result \t\t\t CPU results: $CPU_result");
     if (user_result > CPU_result) {
-      print("User resuls: $user_result \t\t CPU results: $CPU_result");
       print('User wins the round');
-      user_score + 1;
+      user_score = user_score + 1;
     } else if (user_result < CPU_result) {
-      print("User resuls: $user_result \t\t CPU results: $CPU_result");
       print('CPU wins the round');
-      CPU_score + 1;
+      CPU_score = CPU_score + 1;
     } else {
-      print("User resuls: $user_result \t\t CPU results: $CPU_result");
       print('Draw');
     }
-    String? name = stdin.readLineSync();
+    String? next_round = stdin.readLineSync();
   }
   if (user_score > CPU_score) {
     print('User wins the game');
